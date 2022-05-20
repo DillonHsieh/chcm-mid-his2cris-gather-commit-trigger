7b866af8e683a41ac18c3aca7579b4698d58cc9b
DillonHsieh <dillon.hsieh@tpisoftware.com>
2022-05-20 18:12:21 +0800

    add slf4j

--- src/main/java/chcm/mid/his2cris/gather/commit/trigger/HandlerGatherCommitTrigger.java
+++ src/main/java/chcm/mid/his2cris/gather/commit/trigger/HandlerGatherCommitTrigger.java
@@ -35,7 +35,9 @@ import org.json.JSONObject;
 import com.jayway.jsonpath.JsonPath;
 
 import chcm.mid.his2cris.gather.commit.trigger.dto.CHCM;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class HandlerGatherCommitTrigger{	
 	CheckroomSQL roomSQL = new CheckroomSQL();
 	CustomerSQL custSQL = new CustomerSQL();
@@ -60,7 +62,7 @@ public class HandlerGatherCommitTrigger{
 	        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
 	        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 	        bufferedReader.lines().forEach( line -> {
-	        	System.out.println(line);
+	        	System.out.println(line);	        	
 	        	output.append(line);
 	        });
 	        List<Long> timestampList = JsonPath.read(output.toString(), "$.logEvents[*].timestamp");
@@ -68,8 +70,8 @@ public class HandlerGatherCommitTrigger{
 	        
 	        try {
 		        for (int i = 0; i < messageList.size(); i++) {
-		        	System.out.println(messageList.get(i));
 		        	String commit = getCommitKeyword(messageList.get(i));
+		        	log.info(messageList.get(i));
 		        	String[] splitQuery = new String[2];
 		        	String id = "";
 		        	switch(commit) {
